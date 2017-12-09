3256f465f172ae8840e1f4ccb59ee1e100ec0d13
cmcerlean <cmcerlea@mines.edu>
2017-12-09 15:05:31 -0700

    ADDED dao

---
+++ database/database.php
@@ -0,0 +1,45 @@
+<?php
+include database_info.php;
+
+class CockTailDAO {
+    private $conn;
+
+    public function __construct($servername, $username, $password, $dbname) {
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+    }
+    
+    public function is_valid_user($email, $pass) {
+
+
+        $query = "select * from users where email = ? AND password = ?";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("ss", $email, $pass);
+        $stmt->execute();
+
+        if ($stmt->num_rows == 1) {
+          return TRUE;
+        }
+        return FALSE;
+    }
+
+    function get_cocktails() {
+        $data = [];
+        $sql = "SELECT *  FROM cocktails";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                $data[] = $row; 
+            }
+        }
+        return $data;
+    }
+}
+
+$db = new CockTailDAO($servername, $username, $password, $dbname); 
+?>

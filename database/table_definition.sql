85eec25c0a0337ba16426c024858ff781d48b272
cmcerlean <cmcerlea@mines.edu>
2017-12-09 12:28:05 -0700

    Combined sql/ and database/

--- database/table_definition.sql
+++ database/table_definition.sql
@@ -1,27 +1,25 @@
-DROP TABLE cocktails;
-DROP TABLE users;
-DROP TABLE category;
-
 CREATE TABLE users (
-        name varchar(255),
-        email varchar(255),
-        password varchar(255),
-        PRIMARY KEY (email)
+customer_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(40) NOT NULL,
+password VARCHAR(100) NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+email VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE category (
-        category varchar(255),
-        PRIMARY KEY (category)
+category varchar(255),
+PRIMARY KEY (Category)
 );
 
 CREATE TABLE cocktails (
-        entry_id varchar(255),
-        name varchar(255),
-        creator varchar(255),
-        picture_path varchar(255),
-        category varchar(255),
-        ingredients varchar(255),
-        FOREIGN KEY (category) REFERENCES category(category),
-        FOREIGN KEY (creator) REFERENCES user(email),
-        PRIMARY KEY (entry_id)
+entry_id varchar(255),
+name varchar(255),
+creator varchar(255),
+picture_path varchar(255),
+category varchar(255),
+ingredients varchar(255),
+FOREIGN KEY (category) REFERENCES category(category),
+FOREIGN KEY (creator) REFERENCES user(CustomerId),
+PRIMARY KEY (entry_id)
 );

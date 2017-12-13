18ede02970d4fcb21ac7cce8e93a61a4ad8b8416
cmcerlean <cmcerlea@mines.edu>
2017-12-13 10:59:09 -0700

    Added timeout for inactive users

--- inside/ensure_auth.php
+++ inside/ensure_auth.php
@@ -4,3 +4,10 @@ if (!isset($_SESSION['user'])) {
     // Force user to log in
     header('Location: ../auth.php');
 }
+
+if ($_SESSION['last_activity'] < time() - $_SESSION['expire_time']) {    
+    header('Location: ./logout.php');
+} else {
+    $_SESSION['last_activity'] = time();
+}
+?>

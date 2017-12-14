875e02cf1c86b9e7f255a786084494c237faed79
John Cornish <cornish@mymail.mines.edu>
2017-12-14 09:22:18 -0700

    Session expiration works

--- inside/ensure_auth.php
+++ inside/ensure_auth.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
+$expire_time = 30;
 if (!isset($_SESSION['user'])) {
     // Force user to log in
     header('Location: ../auth.php');
 }
 
-if ($_SESSION['last_activity'] < time() - $_SESSION['expire_time']) {    
+//if ($_SESSION['last_activity'] < time() - $_SESSION['expire_time']) {    
+if (isset($_SESSION['last_activity']) && $_SESSION['last_activity'] < time() - $expire_time) {    
     header('Location: ./logout.php');
 } else {
     $_SESSION['last_activity'] = time();
